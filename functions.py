eb67548d95d30f694390ce0767b3ec1e63d76d7f
raunak <raunakbiswas05@gmail.com>
2024-12-15 12:06:03 +0530

    added a log system

--- functions.py
+++ functions.py
@@ -3,14 +3,28 @@ def get_tasks(filepath="files/tasks.txt"):
         _items = _file.readlines()
         return _items
 
+
 def write_tasks(items_, filepath ="files/tasks.txt"):
     with open(filepath, "w", ) as _file:
         _file.writelines(items_)
+
 
 def get_point(filepath="files/points.txt"):
     with open(filepath, "r") as _file:
         _items = _file.read()
         return _items
+
+
 def write_point(items_, filepath ="files/points.txt"):
     with open(filepath, "w", ) as _file:
         _file.write(items_)
+
+
+def get_logs(filepath = "files/logs.txt"):
+    with open(filepath, "r") as _file:
+        _items = _file.readlines()
+        return _items
+
+def write_logs(items_, filepath ="files/logs.txt"):
+    with open(filepath, "w", ) as _file:
+        _file.writelines(items_)
--- point_system.py
+++ point_system.py
@@ -1,5 +1,6 @@
 import FreeSimpleGUI as Gui
 import edit_function,functions
+import os, time
 
 
 def point_system():
@@ -13,11 +14,12 @@ def point_system():
     main_score = Gui.Text(score,font=("black",20),key="score")
     items  = Gui.Listbox(values=tasks,key="tasks",enable_events=True,size=(45,7))
     notice = Gui.Text("NOTE: relapse or failure in completion of goals will result in -10 points",font=("",7))
-    minus_button = Gui.Button("deduct 10 points", key="minus")
-    edit_items_button = Gui.Button("edit items", key="edit")
+    minus_button = Gui.Button("Deduct 10 points", key="minus")
+    edit_items_button = Gui.Button("Edit items", key="edit")
+    lobs_button = Gui.Button("Logs", key="logs")
 
 
-    window = Gui.Window("Discipline app", layout=[[score_text,main_score],[items,edit_items_button],[notice],[minus_button]],font=("Helvetica",10))
+    window = Gui.Window("Discipline app", layout=[[score_text,main_score],[items,edit_items_button],[notice],[minus_button,lobs_button]],font=("Helvetica",10))
 
     while True:
         score = functions.get_point()
@@ -39,6 +41,11 @@ def point_system():
                 functions.write_point(str(int_score))
                 window["score"].update(str(int_score))
 
+                current_logs = functions.get_logs()
+                new_log = f"log: {time.strftime("%b %d %Y, %H:%M:%S")} : {value["tasks"][0]}"
+                current_logs.append(new_log)
+                functions.write_logs(current_logs)
+
             case "minus":
                 int_score -= 10
                 functions.write_point(str(int_score))
@@ -48,6 +55,10 @@ def point_system():
             case "edit":
                 window.close()
                 edit_function.edit_function()
+
+            case "logs":
+                filepath = "files/logs.txt"
+                os.system(f"notepad {filepath}")
 
 
     window.close()
